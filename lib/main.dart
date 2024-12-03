5542cfb433b5b02b2c05932376e91d5b7b7cd941
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2024-12-03 17:59:38 +0200

    add NoteItem widget and integrate with NotesViewBody; update theme to use Poppins font and add font_awesome_flutter dependency

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        home: const NotesView());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
+      ),
+      home: const NotesView(),
+    );
   }
 }
