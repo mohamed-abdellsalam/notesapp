790c69d45fb4d13b806354a0baf508fa6cb71747
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2024-12-03 19:54:15 +0200

    feat: enhance CustomTextField with hint and maxLines; update AddNoteBottomSheet to include title and content fields

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -6,14 +6,23 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'Tilte',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
+          )
         ],
       ),
     );
